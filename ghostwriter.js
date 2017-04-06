5528f2f0a02c29f707c2f7b8640474d9bbb75f5d
Unknown <ogauper@mail.csuchico.edu>
2017-04-06 11:54:25 -0700

    Random sentence

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,18 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["What's the deal with", "Have you ever noticed", "I don't understand", "There's nothing I hate more than"];
+  var body = ["airplance food.", "the weather.", "Donald Trump.", "television.", "Chico State." ];
+  var end = ["It's way too hot.", "It smells weird."];
+  randIndex = randomUpTo(3);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(4);
+  sentence = sentence + " " + body[randIndex];
+  randIndex = randomUpTo(1);
+  sentence = sentence + " " + end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
+
 }
 
 //Generates a random whole number between 0 and max (inclusive)
